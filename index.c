4a58a31a3f4d5d1a69e97294cd0fc65214a9f682
José Lucas <jlgf.profissional@gmail.com>
2025-11-13 19:50:41 -0300

    feat: get window size

--- index.c
+++ index.c
@@ -4,25 +4,62 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct editorConfig {
-    struct termios E.orig_termios;
-}
+    struct termios orig_termios;
+    int screenrows;
+    int screencols;
+};
 
 struct editorConfig E;
-
-void editorDrawRows(){
-    int y;
-    for(y = 0; y < 24; y++){
-        write(STDOUT_FILENO, "~\r\n", 3);
-    }
-}
 
 void refreshScreen(){
     write(STDIN_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
+}
+void kys(const char *s){
+    refreshScreen();
+
+    perror(s);
+    exit(1);
+}
+
+char editorReadKey(){
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1){
+        if(nread == -1 && errno != EAGAIN) kys("read");
+    }
+
+    return c;
+}
+
+int getWindowSize(int *rows, int *cols){
+    struct winsize ws;
+
+    if(1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+        if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        editorReadKey();
+        return -1;
+    }else{
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+void init(){
+    if(getWindowSize(&E.screenrows, &E.screencols) == -1) kys("getWindowSize");
+}
+
+void editorDrawRows(){
+    int y;
+    for(y = 0; y < E.screenrows; y++){
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
 }
 
 void editorRefreshScreen(){
@@ -30,13 +67,6 @@ void editorRefreshScreen(){
     editorDrawRows();
 
     write(STDOUT_FILENO, "\x1b[H", 3);
-}
-
-void kys(const char *s){
-    refreshScreen();
-
-    perror(s);
-    exit(1);
 }
 
 void disableRawMode(){
@@ -58,16 +88,6 @@ void enableRawMode(){
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) kys("tcsetattr");
 }
 
-char editorReadKey(){
-    int nread;
-    char c;
-    while ((nread = read(STDIN_FILENO, &c, 1)) != 1){
-        if(nread == -1 && errno != EAGAIN) kys("read");
-    }
-
-    return c;
-}
-
 void editorProcessKeypress(){
     char c = editorReadKey();
 
@@ -79,8 +99,8 @@ void editorProcessKeypress(){
 }
 
 int main (){
-
     enableRawMode();
+    init();
 
     while(1){
         editorRefreshScreen();

9943318645d0d53be9d382a3701a622f0dc286c7
José Lucas <jlgf.profissional@gmail.com>
2025-11-16 23:19:42 -0300

    feat: status bar

--- index.c
+++ index.c
@@ -35,6 +35,7 @@ struct editorConfig {
     erow *row;
     int rowoff;
     int coloff;
+    char *filename;
 };
 
 struct editorConfig E;
@@ -251,6 +252,9 @@ void editorAppendRow(char *s, size_t len){
 }
 
 void open(char *filename){
+    free(E.filename);
+    E.filename = strdup(filename);
+
     FILE *fp = fopen(filename, "r");
     if(!fp) kys("fopen");
 
@@ -275,8 +279,10 @@ void init(){
     E.row = NULL;
     E.rowoff = 0;
     E.coloff = 0;
+    E.filename = NULL;
 
     if(getWindowSize(&E.screenrows, &E.screencols) == -1) kys("getWindowSize");
+    E.screenrows -= 1;
 }
 
 void scroll(){
@@ -324,10 +330,26 @@ void editorDrawRows(struct appendingBuff *ab){
         }
         
         abAppend(ab, "\x1b[K", 3);
-        if(y < E.screenrows - 1){
-            abAppend(ab, "\r\n", 2);
+        abAppend(ab, "\r\n", 2);
+    }
+}
+
+void statusBar(struct appendingBuff *ab){
+    abAppend(ab, "\x1b[7m", 4);
+    char status[80], rstatus[80];
+    int len = snprintf(status, sizeof(status), "%20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+    if(len > E.screencols) len = E.screencols;
+    while(len < E.screencols){
+        if(E.screencols - len == rlen){
+            abAppend(ab, rstatus, rlen);
+            break;
+        }else{
+            abAppend(ab, " ", 1);
+            len++;
         }
     }
+    abAppend(ab, "\x1b[m", 3);
 }
 
 void editorRefreshScreen(){
@@ -339,6 +361,7 @@ void editorRefreshScreen(){
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
+    statusBar(&ab);
 
     char buf[32];
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);

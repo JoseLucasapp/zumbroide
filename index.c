443de93250b976c771bd02d2b6c9ba8c03f9a868
José Lucas <jlgf.profissional@gmail.com>
2025-11-16 23:35:57 -0300

    fix: update status bar

--- index.c
+++ index.c
@@ -341,10 +341,11 @@ void editorDrawRows(struct appendingBuff *ab){
 void statusBar(struct appendingBuff *ab){
     abAppend(ab, "\x1b[7m", 4);
     char status[80], rstatus[80];
-    int len = snprintf(status, sizeof(status), "%20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
     int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
     
     if(len > E.screencols) len = E.screencols;
+    abAppend(ab, status, len);
     
     while(len < E.screencols){
         if(E.screencols - len == rlen){
